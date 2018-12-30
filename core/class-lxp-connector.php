18d646cad91b0e97305ba7dfa1d7e379c55e2900
valovenko <alina.valovenko@gmail.com>
2018-12-30 12:02:01 +0200

    created post via api

--- core/class-lxp-connector.php
+++ core/class-lxp-connector.php
@@ -4,11 +4,71 @@ class Lxp_Connector {
 	public $data;
 
 	public function __construct( $api_url ) {
-		$this->data = $this->get_xml_by_url( $api_url );
+		$this->data = $this->get_entries_by_api( $api_url );
 	}
 
-	protected function get_xml_by_url( $api_url ) {
-		$content_xml = file_get_contents( $api_url );
-		return $content_xml;
+	protected function get_entries_by_api( $api_url ) {
+		$content_xml   = file_get_contents( $api_url );
+		$lotteries_xml = new SimpleXMLElement( $content_xml );
+		$lotteries_arr = json_decode( json_encode( $lotteries_xml ), true );
+		$this->convert_antries_to_post_type( $lotteries_arr['entry'] );
+
+		return $lotteries_arr['entry'];
 	}
+
+	protected function convert_antries_to_post_type( $entries = null ) {
+		foreach ( $entries as $id => $entry ) {
+			$lottery_id           = $entry['lottery_id'];
+			$title                = $entry['title'];
+			$published            = $entry['published'];
+			$updated              = $entry['updated'];
+			$post_content         = $entry['content'];
+			$link_id              = $entry['id'];
+			$lottery_logo         = $entry['lottery_logo'];
+			$last_draw_date       = $entry['last_draw_date'];
+			$last_draw_results    = $entry['last_draw_results'];
+			$next_draw_date       = $entry['next_draw_date'];
+			$next_draw_jackpot    = $entry['next_draw_jackpot'];
+			$next_draw_close_date = $entry['next_draw_close_date'];
+			$play_link            = $entry['play_link'];
+			$args                 = array(
+				'posts_per_page' => - 1,
+				'post_type'      => 'entry',
+				'meta_query'    => array(
+					array(
+						'key'       => 'lottery_id',
+						'value'     => $lottery_id,
+						'compare'   => 'LIKE',
+					),
+			));
+			$post                 = get_posts( $args );
+			if ( ! empty( $post ) ) {
+				/*update here*/
+			} else {
+				$post_data = array(
+					'post_title'   => wp_strip_all_tags( $title ),
+					'post_content' => $post_content,
+					'post_status'  => 'publish',
+					'post_author'  => 1,
+					'post_date'    => $published,
+					'post_type'    => 'entry',
+					'meta_input'   => array(
+						'lottery_id'           => $lottery_id,
+						'link_id'              => $link_id,
+						'lottery_logo'         => $lottery_logo,
+						'last_draw_date'       => $last_draw_date,
+						'last_draw_results'    => $last_draw_results,
+						'next_draw_date'       => $next_draw_date,
+						'next_draw_jackpot'    => $next_draw_jackpot,
+						'next_draw_close_date' => $next_draw_close_date,
+						'play_link'            => $play_link
+					),
+				);
+
+				$post_id = wp_insert_post( $post_data );
+
+			}
+		}
+	}
+
 }
--- va-loterias-parser.php
+++ va-loterias-parser.php
@@ -28,11 +28,14 @@ if ( ! class_exists( "Loterias_XML_Parser" ) ) {
             register_deactivation_hook(LXP_NAME, array($this, 'deactivate'));
             register_uninstall_hook( LXP_NAME, array( &$this, 'uninstall' ) );
 
+	        add_action('init', array($this,'lxp_register_post_type'));
+	        add_action('add_meta_boxes', array($this, 'lxp_add_custom_fields'), 1);
+
 	        $page = new LXP_Admin();
         }
         
         public function activate(){
-        	
+
         }
 
 	    public function deactivate(  ) {
@@ -42,6 +45,56 @@ if ( ! class_exists( "Loterias_XML_Parser" ) ) {
 	    public function uninstall(  ) {
 		    
         }
+
+	    function lxp_register_post_type(){
+		    register_post_type('entry', array(
+			    'labels'             => array(
+				    'name'               => 'Entries', // Основное название типа записи
+				    'singular_name'      => 'Entry', // отдельное название записи типа Book
+				    'add_new'            => 'Add new',
+				    'add_new_item'       => 'Add new Entry',
+				    'edit_item'          => 'Edit Entry',
+				    'new_item'           => 'New Entry',
+				    'view_item'          => 'View Entry',
+				    'search_items'       => 'Find Entry',
+				    'menu_name'          => 'Entries'
+
+			    ),
+			    'public'             => false,
+			    'publicly_queryable' => true,
+			    'show_ui'            => true,
+			    'show_in_menu'       => true,
+			    'query_var'          => true,
+			    'rewrite'            => true,
+			    'capability_type'    => 'post',
+			    'has_archive'        => true,
+			    'hierarchical'       => false,
+			    'menu_position'      => null,
+			    'supports'           => array('title','editor','author','thumbnail')
+		    ) );
+	    }
+
+	    function lxp_add_custom_fields(){
+		    add_meta_box( 'extra_fields', 'Entry properties', array($this, 'entry_properties_callback'), 'entry', 'normal', 'high'  );
+
+	    }
+
+	    function entry_properties_callback($post){
+		    ?>
+		    <div>
+			    <label>Lottery id: <input type="text" name="entry_properties[lottery_id]" value="<?php echo get_post_meta($post->ID, 'lottery_id', 1); ?>" /></label>
+			    <label>Lottery id (link): <input type="text" name="entry_properties[link_id]" value="<?php echo get_post_meta($post->ID, 'link_id', 1); ?>" /></label>
+			    <label>Lottery logo: <input type="text" name="entry_properties[lottery_logo]" value="<?php echo get_post_meta($post->ID, 'lottery_logo', 1); ?>" /></label>
+			    <label>Last draw date: <input type="text" name="entry_properties[last_draw_date]" value="<?php echo get_post_meta($post->ID, 'last_draw_date', 1); ?>" /></label>
+			    <label>Last draw results: <input type="text" name="entry_properties[last_draw_results]" value="<?php echo get_post_meta($post->ID, 'last_draw_results', 1); ?>" /></label>
+			    <label>Next draw date: <input type="text" name="entry_properties[last_draw_results]" value="<?php echo get_post_meta($post->ID, 'next_draw_date', 1); ?>" /></label>
+			    <label>Next draw jackpot: <input type="text" name="entry_properties[next_draw_jackpot]" value="<?php echo get_post_meta($post->ID, 'next_draw_jackpot', 1); ?>" /></label>
+			    <label>Next draw close date: <input type="text" name="entry_properties[next_draw_close_date]" value="<?php echo get_post_meta($post->ID, 'next_draw_close_date', 1); ?>" /></label>
+			    <label>Play link: <input type="text" name="entry_properties[play_link]" value="<?php echo get_post_meta($post->ID, 'play_link', 1); ?>" /></label>
+
+		    </div>
+		    <?php
+	    }
     }
 
     new Loterias_XML_Parser();
